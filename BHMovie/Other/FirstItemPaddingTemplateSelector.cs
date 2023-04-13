d42319372c8ba86eb217212a1b8be623ffbfa4ad
Faris Rizvanovic <faris.rizvanovic00@gmail.com>
2023-04-13 13:47:01 +0200

    FirstItemTemplateSelector for favourite movies layout design

---
+++ BHMovie/Other/FirstItemPaddingTemplateSelector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BHMovie.Model;
+
+namespace BHMovie.Other
+{
+    public class FirstItemPaddingTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate FirstItemTemplate { get; set; }
+        public DataTemplate DefaultItemTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var collectionView = (CollectionView)container;
+            var isFirstItem = ((ObservableCollection<Movie>)collectionView.ItemsSource)?.IndexOf(item as Movie) == 0;
+
+            return isFirstItem ? FirstItemTemplate : DefaultItemTemplate;
+        }
+    }
+}
+
